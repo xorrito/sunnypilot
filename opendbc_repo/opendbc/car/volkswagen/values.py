6f2614db26b0bce374e9041cfcdc277073f01480
Xorrito Del Bosque <1356750+xorrito@users.noreply.github.com>
2025-06-29 15:21:29 -0700

    Update values.py
    removed minsteer and lowered minspeed to 16

--- opendbc_repo/opendbc/car/volkswagen/values.py
+++ opendbc_repo/opendbc/car/volkswagen/values.py
@@ -301,7 +301,7 @@ class CAR(Platforms):
   )
   VOLKSWAGEN_PASSAT_NMS = VolkswagenPQPlatformConfig(
     [VWCarDocs("Volkswagen Passat NMS 2017-22")],
-    VolkswagenCarSpecs(mass=1503, wheelbase=2.80, minSteerSpeed=50 * CV.KPH_TO_MS, minEnableSpeed=20 * CV.KPH_TO_MS),
+    VolkswagenCarSpecs(mass=1503, wheelbase=2.80, minEnableSpeed=16 * CV.KPH_TO_MS),
     chassis_codes={"A3"},
     wmis={WMI.VOLKSWAGEN_USA_CAR},
   )
